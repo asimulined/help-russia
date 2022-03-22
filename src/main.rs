460f578a654a2b7fff588c77000b3ee5d9429ef2
SkwalExe <support@skwal.net>
2022-03-22 06:18:38 +0100

    💠 removed unused variables

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 // Author : SkwalExe
 // Github : https://github.com/SkwalExe
 
-#![allow(dead_code)]
-
 extern crate reqwest;
 use crossterm_cursor::TerminalCursor;
 use std::io::Write;
@@ -14,15 +12,10 @@ const GREEN: &str = "\x1b[92m";
 const YELLOW: &str = "\x1b[93m";
 const BLUE: &str = "\x1b[94m";
 const MAGENTA: &str = "\x1b[95m";
-const CYAN: &str = "\x1b[96m";
 const WHITE: &str = "\x1b[97m";
 const RESET: &str = "\x1b[0m";
 const BG_RED: &str = "\x1b[41m";
-const BG_GREEN: &str = "\x1b[42m";
-const BG_YELLOW: &str = "\x1b[43m";
-const BG_BLUE: &str = "\x1b[44m";
 const BG_MAGENTA: &str = "\x1b[45m";
-const BG_CYAN: &str = "\x1b[46m";
 
 fn print_wait(text: &str) {
     std::thread::sleep(std::time::Duration::from_millis(100));
